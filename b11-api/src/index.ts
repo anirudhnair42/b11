3e52cbdb602180335d624ef83f27c4263f21aea7
Anirudh Nair <anirudhnair42@gmail.com>
2023-08-22 21:46:43 -0700

    AN-009: Updating Vercel CORS config

--- b11-api/src/index.ts
+++ b11-api/src/index.ts
@@ -32,15 +32,6 @@ app.use(
     allowedHeaders: ["Content-Type", "authorization"],
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
 
 app.use(cookieParser());
 app.use(bodyParser.json());
