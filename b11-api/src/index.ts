c73ded8a64acc0bb626db2fe8fb977fc6eb9107b
Anirudh Nair <anirudhnair42@gmail.com>
2023-08-22 22:47:22 -0700

    AN-013: Handling options response

--- b11-api/src/index.ts
+++ b11-api/src/index.ts
@@ -33,14 +33,8 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
+app.options("*", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.use(cookieParser());
