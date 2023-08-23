0de4de80d35710238c3de7aadeadee910fb3ae3d
Anirudh Nair <anirudhnair42@gmail.com>
2023-08-22 21:52:20 -0700

    AN-010: Updating Express CORS config

--- b11-api/src/index.ts
+++ b11-api/src/index.ts
@@ -33,6 +33,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(cookieParser());
 app.use(bodyParser.json());
 
